b13f5d02fc8f07d6f1100bedad396f8b42661cfa
SDM_Student <rohitbadi04@gmail.com.in>
2025-08-03 15:58:34 +0530

    made changes

--- Backend/routers/auth.js
+++ Backend/routers/auth.js
@@ -9,7 +9,7 @@ router.post("/login", async (req, res) => {
     const user = await User.findOne({ username, password });
     if (!user) return res.status(401).json({ error: "Invalid credentials" });
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "5s" });
     res.json({ token });
 });
 
